37a2ffc51c9d7df0c1afc6c48b6a7d8cd573705c
Angel <araozangel842@gmail.com>
2022-11-06 22:20:32 +0200

    Generate devise configuration for User and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # GET /about
   get "about", to: "about#index"
 
-  get "sign_up", to: "registration#new"
+  # get "sign_up", to: "registration#new"
 
   root to: "main#index"
 
