0a7f25a85269dfde13833de0111611b110fad852
Angel <araozangel842@gmail.com>
2022-11-07 11:39:31 +0200

    Remove password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_06_192053) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_093526) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
