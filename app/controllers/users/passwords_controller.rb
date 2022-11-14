214bafe109c2d0d13436c4f14228fe242281eded
Angel <araozangel842@gmail.com>
2022-11-14 12:06:55 +0200

    Fix error on forgot-password

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  skip_after_action :verify_authorized
   # GET /resource/password/new
   # def new
   #   super
